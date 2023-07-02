3a61484a813ec5ac03cf3ad14c9f010a4fb75fea
Your Name <you@example.com>
2023-07-02 17:34:08 +0530

    function in TS part-2

--- basics1/functions.js
+++ basics1/functions.js
@@ -1,4 +1,5 @@
 "use strict";
+//function in typescript (part-1)//
 Object.defineProperty(exports, "__esModule", { value: true });
 //Example-1 ( Accepting only number data type )
 function subTwo(num) {
@@ -18,3 +19,24 @@ function logIn(name, password, isPaid) {
     if (isPaid === void 0) { isPaid = false; }
 }
 logIn("s", "remind");
+//function in typescript (part-2)//
+//Example-1 string-> Mentiond what data type should return this function
+var greet = function (name) {
+    return "Hello " + name;
+};
+greet("sarvesh");
+//Example-2 string-> Mentiond what data type should return this map function
+var villans = ["saniyan sakkada", "pattasu balu", "muthu pandi"];
+villans.map(function (villan) {
+    return "my villan ".concat(villan);
+});
+//Example-3 void-> Mentiond what data type should return this function
+function consoleError(errmsg) {
+    console.log(errmsg);
+}
+consoleError("void");
+//Example-4 never-> Mentiond what data type should return this function
+function handleError(errmsg) {
+    throw new Error(errmsg);
+}
+handleError("error");
--- basics1/functions.ts
+++ basics1/functions.ts
@@ -1,3 +1,5 @@
+//function in typescript (part-1)//
+
 //Example-1 ( Accepting only number data type )
 function subTwo(num: number) {
   return num - 2;
@@ -26,6 +28,36 @@ signUpUser("s", "s@yesd.com", "forgot", false);
 function logIn(name: string, password: string, isPaid: boolean = false) {}
 
 logIn("s", "remind");
+
+//function in typescript (part-2)//
+
+//Example-1 string-> Mentiond what data type should return this function
+const greet = (name: string): string => {
+  return "Hello " + name;
+};
+
+greet("sarvesh");
+
+//Example-2 string-> Mentiond what data type should return this map function
+let villans = ["saniyan sakkada", "pattasu balu", "muthu pandi"];
+
+villans.map((villan: string): string => {
+  return `my villan ${villan}`;
+});
+
+//Example-3 void-> Mentiond what data type should return this function
+function consoleError(errmsg: string): void {
+  console.log(errmsg);
+}
+
+consoleError("void");
+
+//Example-4 never-> Mentiond what data type should return this function
+function handleError(errmsg: string): never {
+  throw new Error(errmsg);
+}
+
+handleError("error");
 
 //temporary fixing for squiggling error
 export {};
